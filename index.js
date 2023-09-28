0896c45ba1eff3129b3d86bc3bfc3c3990248766
Rowland <rowland.wanderi@student.moringaschool.com>
2023-09-28 10:54:40 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 function addingEventListener() {
-}
+    const input = document.getElementById("input");
+    function clickAlert() {
+      alert("I was clicked!");
+    }
+    console.log(input.addEventListener("click", clickAlert))
+  }
+input.addEventListener('click', addingEventListener())
